1976eff04322786882df7c1cee3b115e721521e4
mariamosmann <rossimosmann@gmail.com>
2019-01-17 22:16:14 -0500

    Added the forms for the other sections  :computer:

--- src/App.js
+++ src/App.js
@@ -85,18 +85,74 @@ class App extends Component {
 
 					<section className="dramas">
 						<h2 className="dramas__h2 heading__h2">Dramas</h2>
+
+						<form onSubmit={this.handleSubmit} action="" className="dramas__form form">
+							<label htmlFor="drama" className="form__label visuallyhidden">Drama title.</label>
+							<input
+								type="text"
+								className="form__field"
+								onChange={this.handleChange}
+								id="drama"
+								value={this.state.drama}
+								placeholder="Drama title"
+							/>
+
+							<input type="submit" className="form__submit button" value="Add drama." />
+						</form>
 					</section>
 
 					<section className="theatre">
 						<h2 className="theatre__h2 heading__h2">Theatre</h2>
+
+						<form onSubmit={this.handleSubmit} action="" className="theatre__form form">
+							<label htmlFor="play" className="form__label visuallyhidden">Play title.</label>
+							<input
+								type="text"
+								className="form__field"
+								onChange={this.handleChange}
+								id="play"
+								value={this.state.play}
+								placeholder="Play title"
+							/>
+
+							<input type="submit" className="form__submit button" value="Add play." />
+						</form>
 					</section>
 
 					<section className="games">
 						<h2 className="games__h2 heading__h2">Games</h2>
+
+						<form onSubmit={this.handleSubmit} action="" className="games__form form">
+							<label htmlFor="game" className="form__label visuallyhidden">Game title.</label>
+							<input
+								type="text"
+								className="form__field"
+								onChange={this.handleChange}
+								id="game"
+								value={this.state.game}
+								placeholder="Game title"
+							/>
+
+							<input type="submit" className="form__submit button" value="Add game." />
+						</form>
 					</section>
 
 					<section className="books">
 						<h2 className="books__h2 heading__h2">Books</h2>
+
+						<form onSubmit={this.handleSubmit} action="" className="books__form form">
+							<label htmlFor="book" className="form__label visuallyhidden">Book title.</label>
+							<input
+								type="text"
+								className="form__field"
+								onChange={this.handleChange}
+								id="book"
+								value={this.state.book}
+								placeholder="Book title"
+							/>
+
+							<input type="submit" className="form__submit button" value="Add book." />
+						</form>
 					</section>
 				</main>
 			</div>
