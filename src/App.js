cc40e16bf8797212341b6349898b1833e7c3849b
mariamosmann <rossimosmann@gmail.com>
2019-01-17 21:12:27 -0500

    Initial commit!  :notes:

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			movies: "",
+			series: "",
+			dramas: "",
+			theatre: "",
+			games: "",
+			books: "" 
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="header">
+					<h1 className="header__h1 heading__h1">✨ My App! ✨</h1>
+				</header>
+
+				<main className="main">
+					<section className="movies">
+						<h2 className="movies__h2 heading__h2">Movies</h2>
+					</section>
+
+					<section className="series">
+						<h2 className="series__h2 heading__h2">Series</h2>
+					</section>
+
+					<section className="dramas">
+						<h2 className="dramas__h2 heading__h2">Dramas</h2>
+					</section>
+
+					<section className="theatre">
+						<h2 className="theatre__h2 heading__h2">Theatre</h2>
+					</section>
+
+					<section className="games">
+						<h2 className="games__h2 heading__h2">Games</h2>
+					</section>
+
+					<section className="books">
+						<h2 className="books__h2 heading__h2">Books</h2>
+					</section>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
