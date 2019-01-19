175248b2393793f5fdd17750cbf0bf14a47de738
mariamosmann <rossimosmann@gmail.com>
2019-01-18 22:33:53 -0500

    Added counter section, made emojis accessible :computer:

--- src/App.js
+++ src/App.js
@@ -43,116 +43,152 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="header">
-					<h1 className="header__h1 heading__h1">✨ My App! ✨</h1>
+					<h1 className="header__h1 heading__h1"><span className="header__emoji emoji" aria-label="sparkles" role="img">✨</span>  My Tracking App! <span className="header__emoji emoji" aria-label="sparkles" role="img">✨</span></h1>
 				</header>
 
 				<main className="main">
 					<section className="movies">
-						<h2 className="movies__h2 heading__h2">Movies</h2>
+						<div className="movies__wrapper wrapper">
+							<h2 className="movies__h2 heading__h2">Movies <span className="movies__emoji emoji" aria-label="Clapper Board" role="img">🎬</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="movies__form form">
-							<label htmlFor="movie" className="form__label visuallyhidden">Movie title.</label>
-							<input 
-							type="text" 
-							className="form__field"
-							onChange={this.handleChange}
-							id="movie"
-							value={this.state.movie}
-							placeholder="Movie title"
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="movies__form form">
+								<label htmlFor="movie" className="form__label visuallyhidden">Movie title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="movie"
+									value={this.state.movie}
+									placeholder="Movie title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add movie."/>
-						</form>
+								<input type="submit" className="form__submit button" value="Add movie." />
+							</form>
+						</div>
 					</section>
 
 					<section className="series">
-						<h2 className="series__h2 heading__h2">Series</h2>
+						<div className="series__wrapper wrapper">
+							<h2 className="series__h2 heading__h2">Series <span className="series__emoji emoji" aria-label="Television" role="img">📺</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="series__form form">
-							<label htmlFor="series" className="form__label visuallyhidden">Series title.</label>
-							<input 
-							type="text"
-							className="form__field"
-							onChange={this.handleChange}
-							id="series"
-							value={this.state.series}
-							placeholder="Series title" 
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="series__form form">
+								<label htmlFor="series" className="form__label visuallyhidden">Series title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="series"
+									value={this.state.series}
+									placeholder="Series title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add series."/>
-						</form>
+								<input type="submit" className="form__submit button" value="Add series." />
+							</form>
+						</div>
 					</section>
 
 					<section className="dramas">
-						<h2 className="dramas__h2 heading__h2">Dramas</h2>
+						<div className="dramas__wrapper wrapper">
+							<h2 className="dramas__h2 heading__h2">Dramas <span className="dramas__emoji emoji" aria-label="Couple With Heart" role="img">💑</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="dramas__form form">
-							<label htmlFor="drama" className="form__label visuallyhidden">Drama title.</label>
-							<input
-								type="text"
-								className="form__field"
-								onChange={this.handleChange}
-								id="drama"
-								value={this.state.drama}
-								placeholder="Drama title"
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="dramas__form form">
+								<label htmlFor="drama" className="form__label visuallyhidden">Drama title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="drama"
+									value={this.state.drama}
+									placeholder="Drama title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add drama." />
-						</form>
+								<input type="submit" className="form__submit button" value="Add drama." />
+							</form>
+						</div>
 					</section>
 
 					<section className="theatre">
-						<h2 className="theatre__h2 heading__h2">Theatre</h2>
+						<div className="theatre__wrapper wrapper">
+							<h2 className="theatre__h2 heading__h2">Theatre <span className="theatre__emoji emoji" aria-label="Drama Masks" role="img">🎭</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="theatre__form form">
-							<label htmlFor="play" className="form__label visuallyhidden">Play title.</label>
-							<input
-								type="text"
-								className="form__field"
-								onChange={this.handleChange}
-								id="play"
-								value={this.state.play}
-								placeholder="Play title"
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="theatre__form form">
+								<label htmlFor="play" className="form__label visuallyhidden">Play title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="play"
+									value={this.state.play}
+									placeholder="Play title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add play." />
-						</form>
+								<input type="submit" className="form__submit button" value="Add play." />
+							</form>
+						</div>
 					</section>
 
 					<section className="games">
-						<h2 className="games__h2 heading__h2">Games</h2>
+						<div className="games__wrapper wrapper">
+							<h2 className="games__h2 heading__h2">Games <span className="games__emoji emoji" aria-label="Video Game" role="img">🎮</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="games__form form">
-							<label htmlFor="game" className="form__label visuallyhidden">Game title.</label>
-							<input
-								type="text"
-								className="form__field"
-								onChange={this.handleChange}
-								id="game"
-								value={this.state.game}
-								placeholder="Game title"
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="games__form form">
+								<label htmlFor="game" className="form__label visuallyhidden">Game title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="game"
+									value={this.state.game}
+									placeholder="Game title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add game." />
-						</form>
+								<input type="submit" className="form__submit button" value="Add game." />
+							</form>
+						</div>
 					</section>
 
 					<section className="books">
-						<h2 className="books__h2 heading__h2">Books</h2>
+						<div className="books__wrapper wrapper">
+							<h2 className="books__h2 heading__h2">Books <span className="books__emoji emoji" aria-label="Books" role="img">📚</span></h2>
 
-						<form onSubmit={this.handleSubmit} action="" className="books__form form">
-							<label htmlFor="book" className="form__label visuallyhidden">Book title.</label>
-							<input
-								type="text"
-								className="form__field"
-								onChange={this.handleChange}
-								id="book"
-								value={this.state.book}
-								placeholder="Book title"
-							/>
+							<form onSubmit={this.handleSubmit} action="" className="books__form form">
+								<label htmlFor="book" className="form__label visuallyhidden">Book title.</label>
+								<input
+									type="text"
+									className="form__field"
+									onChange={this.handleChange}
+									id="book"
+									value={this.state.book}
+									placeholder="Book title"
+								/>
 
-							<input type="submit" className="form__submit button" value="Add book." />
-						</form>
+								<input type="submit" className="form__submit button" value="Add book." />
+							</form>
+						</div>
+					</section>
+
+					<section className="counters">
+						<div className="counters__wrapper wrapper">
+							<h2 className="counters__h2 heading__h2">Counters</h2>
+
+							<div className="counters__item">
+								<h3 className="counters__h3 heading__h3">Volunteered <span className="counters__emoji emoji" aria-label="Woman Technologist" role="img">👩‍💻</span></h3>
+								<p className="counters__times">Counter: <span className="counters__span">0</span></p>
+								<input type="submit" value="I did it!" className="counters__button button"/>
+							</div>
+
+							<div className="counters__item">
+								<h3 className="counters__h3 heading__h3">Wrote an Article <span className="counters__emoji emoji" aria-label="Pencil" role="img">✏️</span></h3>
+								<p className="counters__times">Counter: <span className="counters__span">0</span></p>
+								<input type="submit" value="I did it!" className="counters__button button"/>
+							</div>
+
+							<div className="counters__item">
+								<h3 className="counters__h3 heading__h3">Went to a Tech Event <span className="counters__emoji emoji" aria-label="Laptop computer" role="img">💻</span></h3>
+								<p className="counters__times">Counter: <span className="counters__span">0</span></p>
+								<input type="submit" value="I did it!" className="counters__button button"/>
+							</div>
+						</div>
 					</section>
 				</main>
 			</div>
